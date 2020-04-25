284a5ce480eaa099adef04588f71b6987b14644c
Sefa Eyeoglu <contact@scrumplex.net>
2020-04-25 23:37:36 +0200

    meta: add license and notice text

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 /*
+ * vibrantX - Adjust color vibrance of X11 output
+ * Copyright (C) 2020  Sefa Eyeoglu <contact@scrumplex.net> (https://scrumplex.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * vibrantX is based on color-demo-app written by Leo (Sunpeng) Li <sunpeng.li@amd.com>
+ * Original license text of color-demo-app:
+ *
  * Copyright 2018 Advanced Micro Devices, Inc.
- * Copyright 2020 Sefa Eyeoglu <contact@scrumplex.net> (https://scrumplex.net)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -32,20 +52,10 @@
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <X11/extensions/Xrandr.h>
+#include <libdrm/drm_mode.h>  // need drm_color_ctm
 
 #define RANDR_FORMAT 32u
 #define PROP_CTM "CTM"
-
-/**
- * The below data structures are identical to the ones used by DRM. They are
- * here to help us structure the data being passed to the kernel.
- */
-struct drm_color_ctm {
-    /* Transformation matrix in S31.32 format. */
-    uint64_t matrix[9];
-};
-
-
 
 /*******************************************************************************
  * Helper functions
