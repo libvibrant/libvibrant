2065caaf36573e7f0306f75f4fe732a79d263dc9
Sefa Eyeoglu <contact@scrumplex.net>
2020-05-08 10:37:50 +0200

    chore: add license header to .h files

--- vibrant/include/vibrant/ctm.h
+++ vibrant/include/vibrant/ctm.h
@@ -1,6 +1,49 @@
-//
-// Created by scrumplex on 06.05.20.
-//
+/*
+ * vibrant - Adjust color vibrance of X11 output
+ * Copyright (C) 2020  Sefa Eyeoglu <contact@scrumplex.net> (https://scrumplex.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * vibrant is based on color-demo-app written by Leo (Sunpeng) Li <sunpeng.li@amd.com>
+ *
+ * Original license text of color-demo-app:
+ *
+ * Copyright 2018 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER(S) OR AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Authors: AMD
+ *
+ */
 
 #include <stdint.h>
 
@@ -9,9 +52,10 @@
 
 #ifndef VIBRANT_CTM_H
 #define VIBRANT_CTM_H
+
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif // __cplusplus
 
 /**
  * Set a DRM blob property on the given output. It calls XSync at the end to
@@ -108,5 +152,5 @@ int output_has_ctm(Display *dpy, RROutput output);
 
 #ifdef __cplusplus
 }
-#endif
+#endif // __cplusplus
 #endif // VIBRANT_CTM_H
