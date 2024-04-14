7200d4c5063b1d8f429d0d1b1e17c2fcff0d847d
Sefa Eyeoglu <contact@scrumplex.net>
2024-04-14 15:55:59 +0200

    feat: add Nix flake
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      git-hooks,
+      nixpkgs,
+      self,
+      ...
+    }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system} system);
+    in
+    {
+      packages = forAllSystems (
+        pkgs: _: rec {
+          default = libvibrant;
+          libvibrant = pkgs.qt6Packages.callPackage ./nix/package.nix { };
+        }
+      );
+
+      checks = forAllSystems (
+        pkgs: system: {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              clang-format = {
+                enable = true;
+                types_or = [
+                  "c"
+                  "c++"
+                ];
+              };
+              nixfmt = {
+                enable = true;
+                package = self.formatter.${system};
+              };
+            };
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        pkgs: system: {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+
+            inputsFrom = [ self.packages.${system}.libvibrant ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (pkgs: _: pkgs.nixfmt-rfc-style);
+    };
+}
---
+++ nix/package.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  libX11,
+  libXrandr,
+  linuxPackages,
+}:
+
+stdenv.mkDerivation {
+  pname = "libvibrant";
+  version = "2100c09";
+
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions (
+      map (fileName: ../${fileName}) [
+        "cmake"
+        "vibrant"
+        "vibrant-cli"
+        "CMakeLists.txt"
+      ]
+    );
+  };
+
+  buildInputs = [
+    libX11
+    libXrandr
+    linuxPackages.nvidia_x11.settings.libXNVCtrl
+  ];
+  nativeBuildInputs = [ cmake ];
+
+  meta = with lib; {
+    description = "A simple library to adjust color saturation of X11 outputs";
+    homepage = "https://github.com/libvibrant/libvibrant";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ Scrumplex ];
+    mainProgram = "vibrant-cli";
+  };
+}
